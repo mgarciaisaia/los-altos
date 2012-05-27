19ca7fc436d411eb987044228e14d970fc5cb5bd
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-05-27 18:33:02 -0300

    Elimino flush que no hay que implementarla (bah, eso espero)

--- fsc/src/fileSystemClient.c
+++ fsc/src/fileSystemClient.c
@@ -122,34 +122,6 @@ int remote_write(const char *path, const char *input, size_t size, off_t offset,
 	return 0;
 }
 
-/** Possibly flush cached data
- *
- * BIG NOTE: This is not equivalent to fsync().  It's not a
- * request to sync dirty data.
- *
- * Flush is called on each close() of a file descriptor.  So if a
- * filesystem wants to return write errors in close() and the file
- * has cached dirty data, this is a good place to write back data
- * and return any errors.  Since many applications ignore close()
- * errors this is not always useful.
- *
- * NOTE: The flush() method may be called more than once for each
- * open().  This happens if more than one file descriptor refers
- * to an opened file due to dup(), dup2() or fork() calls.  It is
- * not possible to determine if a flush is final, so each flush
- * should be treated equally.  Multiple write-flush sequences are
- * relatively rare, so this shouldn't be a problem.
- *
- * Filesystems shouldn't assume that flush will always be called
- * after some writes, or that if will be called at all.
- *
- * Changed in version 2.2
- */
-int remote_flush(const char *path, struct fuse_file_info *fileInfo) {
-	//FIXME: implementar
-	return 0;
-}
-
 /** Release an open file
  *
  * Release is called when there are no more references to an open
@@ -281,7 +253,6 @@ static struct fuse_operations remote_operations = {
 		.open = remote_open,
 		.read = remote_read,
 		.write = remote_write,
-		.flush = remote_flush,
 		.release = remote_release,
 		.unlink = remote_unlink,
 		.mkdir = remote_mkdir,
