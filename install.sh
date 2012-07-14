95cdb2d1e31152aa19062664fe8af81822c9e148
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-07-14 13:37:59 -0300

    Script para crear el disco

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd
+mkdir Desarrollo
+touch Desarrollo/ext2.disk
+echo "Voy a ejecutar: sudo mkfs.ext2 -F -O none,sparse_super -b 1024 -I 128 Desarrollo/ext2.disk 2000000"
+sudo mkfs.ext2 -F -O none,sparse_super -b 1024 -I 128 Desarrollo/ext2.disk 2000000
