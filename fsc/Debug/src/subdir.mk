d16b70e7106cf84ad46fec0c801d3e51abdcd498
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-07-16 21:04:51 -0300

    Makefiles con rutas relativas

--- fsc/Debug/src/subdir.mk
+++ fsc/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -D_FILE_OFFSET_BITS=64 -I"/home/utnso/workspaces/2012-1c-los-altos/commons" -Imemcached -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -D_FILE_OFFSET_BITS=64 -I"../../commons" -Imemcached -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
