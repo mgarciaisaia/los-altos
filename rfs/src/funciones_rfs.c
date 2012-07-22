bb6326ba0403aae4ebbdcf1d254aeec1cf9a19f1
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-07-21 22:20:18 -0300

    Borro semaforo puntero bloque logico

--- rfs/src/funciones_rfs.c
+++ rfs/src/funciones_rfs.c
@@ -35,7 +35,6 @@ extern t_list * archivos_abiertos;
 static uint32_t nro_de_grupo = 0;
 static pthread_mutex_t  * sem_escribir;
 static pthread_mutex_t  * sem_leer;
-static pthread_mutex_t * sem_ptr_bloque_logico;
 static pthread_mutex_t * sem_liberar_bloque;
 static pthread_mutex_t * sem_pedir_bloque;
 static pthread_mutex_t * sem_crear_dir;
@@ -58,7 +57,6 @@ void init_semaforos(void) {
 
 	sem_escribir = malloc(sizeof(pthread_mutex_t));
 	sem_leer = malloc(sizeof(pthread_mutex_t));
-	sem_ptr_bloque_logico = malloc(sizeof(pthread_mutex_t));
 	sem_liberar_bloque = malloc(sizeof(pthread_mutex_t));
 	sem_pedir_bloque = malloc(sizeof(pthread_mutex_t));
 	sem_crear_dir = malloc(sizeof(pthread_mutex_t));
@@ -70,7 +68,6 @@ void init_semaforos(void) {
 	sem_truncar_archivo = malloc(sizeof(pthread_mutex_t));
 	pthread_mutex_init(sem_escribir, NULL);
 	pthread_mutex_init(sem_leer, NULL);
-	pthread_mutex_init(sem_ptr_bloque_logico, NULL);
 	pthread_mutex_init(sem_liberar_bloque, NULL);
 	pthread_mutex_init(sem_pedir_bloque, NULL);
 	pthread_mutex_init(sem_crear_dir, NULL);
@@ -85,7 +82,6 @@ void init_semaforos(void) {
 void destroy_semaforos(void) {
 	pthread_mutex_destroy(sem_escribir);
 	pthread_mutex_destroy(sem_leer);
-	pthread_mutex_destroy(sem_ptr_bloque_logico);
 	pthread_mutex_destroy(sem_liberar_bloque);
 	pthread_mutex_destroy(sem_pedir_bloque);
 	pthread_mutex_destroy(sem_crear_dir);
@@ -630,7 +626,6 @@ void * getPtrBloqueIndTriple(uint32_t nroBloqueIndireccion,uint32_t nroBloqueLog
  * 		 de bloque lógico dentro del inodo
  */
 uint32_t * getPtrNroBloqueLogicoDentroInodo(struct INode * inodo,uint32_t nroBloqueLogico){
-	pthread_mutex_lock(sem_ptr_bloque_logico);
 	uint32_t * ptrNroBloqueLogico;
 	if (esBloqueDirecto(nroBloqueLogico)){
 		ptrNroBloqueLogico = &(inodo->blocks[nroBloqueLogico]);
@@ -641,7 +636,6 @@ uint32_t * getPtrNroBloqueLogicoDentroInodo(struct INode * inodo,uint32_t nroBlo
 	} else if (esIndireccionTriple(nroBloqueLogico)){
 		ptrNroBloqueLogico = posicionarIndireccionTriple(inodo->iiiblock, nroBloqueLogico);
 	}
-	pthread_mutex_unlock(sem_ptr_bloque_logico);
 	return ptrNroBloqueLogico;
 }
 
