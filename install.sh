1b029e1cb56bdc1b0c5db703b1defc5a768b9b2c
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-07-28 11:53:22 -0300

    Update del script de instalacion

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+$currentDir=$(pwd)
 cd
 mkdir Desarrollo
-touch Desarrollo/ext2.disk
+truncate --size 0 Desarrollo/ext2.disk
 echo "Voy a ejecutar: sudo mkfs.ext2 -F -O none,sparse_super -b 1024 -I 128 Desarrollo/ext2.disk 2000000"
 sudo mkfs.ext2 -F -O none,sparse_super -b 1024 -I 128 Desarrollo/ext2.disk 2000000
 cd
 mkdir -p tmp/fuse
+cd $currentDir
+./compile.sh
