1fdd58bc569948114371c76ac48426924ef09c9b
mgarciaisaia <mgarciaisaia+github@gmail.com>
2012-07-28 10:52:05 -0300

    Update del script de la cache

--- run_rc.sh
+++ run_rc.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-echo "USAGE: \$ run_rc.sh MAX_PARTITION_SIZE CACHE_SIZE MIN_PARTITION_SIZE"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:commons/Debug:memcached-1.6/.libs/:"
+export LD_LIBRARY_PATH="$(pwd)/commons/Debug:$(pwd)/memcached-1.6/.libs/"
 export MALLOC_TRACE="rc/Mtrace.dat"
-./memcached-1.6/.libs/memcached -vv -p 11212 -E rc/Debug/librc.so -I $1 -m $2 -n $3
+if [ $# -eq 2 ]
+then
+	./memcached-1.6/.libs/memcached -vv -p 11212 -E rc/Debug/librc.so -m $1 -n $2
+elif [ $# -eq 3 ]
+then
+	./memcached-1.6/.libs/memcached -vv -p 11212 -E rc/Debug/librc.so -I $1 -m $2 -n $3
+else
+	echo "USAGE:\n\$ run_rc.sh [ MAX_PARTITION_SIZE ] CACHE_SIZE MIN_PARTITION_SIZE"
+fi
